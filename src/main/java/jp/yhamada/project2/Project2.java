e9c676fd4d90efd2ec854d19f36aa232485348a2
hmdyt <konnyaku108@gmail.com>
2022-12-30 02:06:06 +0900

    format

--- src/main/java/jp/yhamada/project2/Project2.java
+++ src/main/java/jp/yhamada/project2/Project2.java
@@ -1,44 +1,43 @@
 package jp.yhamada.project2;
 
 import com.mojang.logging.LogUtils;
+import jp.yhamada.project2.init.BlockInit;
 import jp.yhamada.project2.init.ItemInit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Project2.MOD_ID)
-public class Project2
-{
+public class Project2 {
     public static final String MOD_ID = "project2";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Project2()
-    {
+    public Project2() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for mod loading
         modEventBus.addListener(this::commonSetup);
 
         ItemInit.ITEMS.register(modEventBus);
+        BlockInit.BLOCKS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
@@ -46,19 +45,16 @@ public class Project2
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
@@ -66,6 +62,8 @@ public class Project2
     }
 
     public static class ModCreativeTab extends CreativeModeTab {
+        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, Project2.MOD_ID);
+
         public ModCreativeTab(int index, String label) {
             super(index, label);
         }
@@ -74,7 +72,5 @@ public class Project2
         public ItemStack makeIcon() {
             return new ItemStack(ItemInit.MIMI_MUSHI.get());
         }
-
-        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, Project2.MOD_ID);
     }
 }
