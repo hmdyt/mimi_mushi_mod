67d9710c04c23b6b6faa12ab8d18af001f770342
hmdyt <konnyaku108@gmail.com>
2022-12-30 02:42:40 +0900

    add mimi mushi block

---
+++ src/main/java/jp/yhamada/project2/init/BlockInit.java
@@ -0,0 +1,38 @@
+package jp.yhamada.project2.init;
+
+import jp.yhamada.project2.Project2;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class BlockInit {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Project2.MOD_ID);
+
+    public static final RegistryObject<Block> MIMI_MUSHI_BLOCK = BLOCKS.register(
+            "mimi_mushi_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE))
+    );
+
+    @SubscribeEvent
+    public static void onRegisterItems(final RegisterEvent event) {
+        if (event.getRegistryKey().equals(ForgeRegistries.Keys.ITEMS)) {
+            BLOCKS.getEntries().forEach((blockRegistryObject -> {
+                Block block = blockRegistryObject.get();
+                Item.Properties properties = new Item.Properties().tab(Project2.ModCreativeTab.instance);
+                Supplier<Item> blockItemFactory = () -> new BlockItem(block, properties);
+                event.register(ForgeRegistries.Keys.ITEMS, blockRegistryObject.getId(), blockItemFactory);
+            }));
+        }
+    }
+}
