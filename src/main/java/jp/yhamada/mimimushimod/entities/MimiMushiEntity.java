ab01003a68ed2171d3e6b1b7375edf7a7dc9bc73
hmdyt <konnyaku108@gmail.com>
2022-12-31 18:29:39 +0900

    mimi mushi flying

--- src/main/java/jp/yhamada/mimimushimod/entities/MimiMushiEntity.java
+++ src/main/java/jp/yhamada/mimimushimod/entities/MimiMushiEntity.java
@@ -1,22 +1,15 @@
 package jp.yhamada.mimimushimod.entities;
 
-import jp.yhamada.mimimushimod.init.EntityInit;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeMod;
-import org.jetbrains.annotations.Nullable;
 
-public class MimiMushiEntity extends Animal {
-    public MimiMushiEntity(EntityType<? extends Animal> entityType, Level level) {
+public class MimiMushiEntity extends Bee {
+    public MimiMushiEntity(EntityType<? extends Bee> entityType, Level level) {
         super(entityType, level);
     }
 
@@ -24,18 +17,9 @@ public class MimiMushiEntity extends Animal {
         return Mob.createMobAttributes()
                 .add(ForgeMod.ENTITY_GRAVITY.get(), 0.5f)
                 .add(Attributes.MAX_HEALTH, 25.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.7246D);
+                .add(Attributes.MOVEMENT_SPEED, 0.7246D)
+                .add(Attributes.ATTACK_DAMAGE, 3D)
+                .add(Attributes.FLYING_SPEED, 1.0D);
     }
 
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob mob) {
-        return EntityInit.MIMI_MUSHI.get().create(level);
-    }
-
-    @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(0, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
-    }
 }
